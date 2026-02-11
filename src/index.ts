57e10530c930b25df6d6957629aed20ceb714150
Jeremy Rolls <jrolls@cloudflare.com>
2026-02-10 19:04:51 -0600

    Personalized greeting in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,17 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+  // Add bindings here later (KV, R2, D1, etc.)
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async fetch(request: Request, _env: Env, _ctx: ExecutionContext): Promise<Response> {
+    const url = new URL(request.url);
+    const name = url.searchParams.get("name") ?? "World";
+
+    return new Response(`Hello ${name}!`, {
+      headers: { "content-type": "text/plain; charset=utf-8" },
+    });
+  },
 } satisfies ExportedHandler<Env>;
